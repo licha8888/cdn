ceee7a8b48ceae92233fe12410a5f532249facaa
licha <licha@gmail.com>
2023-06-18 12:33:45 +0800

    js更新

--- bot/static/admin/js/system/groupcat.js
+++ bot/static/admin/js/system/groupcat.js
@@ -17,7 +17,7 @@ define(["jquery", "easy-admin"], function ($, ea) {
             ea.table.render({
                 init: init,
                 cols: [[
-                    {field: 'id', title: 'id', search_ignore: true},
+                    {field: 'id', title: 'ID', search_ignore: true},
                     {field: 'name', title: '分类名称'},
                     {field: 'remark', title: '备注', templet: ea.table.text},
                     {field: 'create_time', title: '创建时间', search:'range'},
