92d408a98ecd8adde16b9cbab7522614b45e222f
licha <licha@gmail.com>
2023-06-18 12:26:04 +0800

    js更新

--- bot/static/admin/js/system/group.js
+++ bot/static/admin/js/system/group.js
@@ -18,29 +18,55 @@ define(["jquery", "easy-admin"], function ($, ea) {
                 init: init,
                 toolbar: ['refresh', 'export'],
                 cols: [[
-                    {width: 180, field: 'groupname', title: '群名称'},
+                    {width: 150, field: 'groupname', title: '群名称'},
+                    {width: 75, field: 'category.name', title: '群分类', templet:
+                        function (data, option) {
+                            return data.category ? data.category.name : '未分类'
+                        },
+                    },
                     {field: 'ownernickname', title: '权限人'},
                     {field: 'botnickname', title: '机器人'},
                     {width: 90, field: 'depositcount', title: '入款笔数', templet:
                         function (data, option) {                            
-                            return data.depositcount + " / " + data.adjustdepositcount
+                            return data.depositcount // + " / " + data.adjustdepositcount
                         },
                         search_ignore: true
                     },
                     {width: 90, field: 'withdrawcount', title: '下发笔数', templet:
                         function (data, option) {                            
-                            return data.withdrawcount + " / " + data.adjustwithdrawcount
+                            return data.withdrawcount //+ " / " + data.adjustwithdrawcount
                         },
                         search_ignore: true
                     },
-                    {width: 105, field: 'totaldeposit', title: '总入款', search_ignore: true},
-                    {width: 105, field: 'totalwithdraw', title: '总下发', search_ignore: true},
-                    {width: 105, field: 'shouldwithdraw', title: '应下发', search_ignore: true},
-                    {width: 60, field: 'fee', title: '费率', search_ignore: true},
-                    {width: 90, field: 'usdt', title: '美元汇率', search_ignore: true},
+                    {width: 90, field: 'totaldeposit', title: '总入款', templet:
+                        function (data, option) {
+                            return parseInt(data.totaldeposit)
+                        },
+                        search_ignore: true},
+                    {width: 90, field: 'totalwithdraw', title: '总下发', templet:
+                        function (data, option) {
+                            return parseInt(data.totalwithdraw)
+                        },
+                        search_ignore: true},
+                    {width: 90, field: 'shouldwithdraw', title: '应下发', templet:
+                        function (data, option) {
+                            return parseInt(data.totaldeposit)
+                        },
+                        search_ignore: true},
+                    {width: 60, field: 'fee', title: '费率', templet:
+                        function (data, option) {
+                            return parseInt(data.fee)
+                        },
+                        search_ignore: true},
+                    {width: 90, field: 'usdt', title: '美元汇率', templet:
+                        function (data, option) {
+                            return parseInt(data.usdt)
+                        },
+                        search_ignore: true},
                     //{width: 95, field: 'status', title: '状态', templet: ea.table.switch, tips: '正常|禁用', templet: ea.table.switch, search_ignore: true},
                     {width: 180, field: 'update_time_text', title: '更新时间', search: 'range'},
                     {title: '操作', templet: ea.table.tool, operat:[
+                        'edit',
                         [
                             {
                                 class: 'layui-btn layui-btn-normal layui-btn-xs',
@@ -49,7 +75,6 @@ define(["jquery", "easy-admin"], function ($, ea) {
                                 url: '/admin/ht/bill',
                             },
                         ],
-                        'edit',
                     ]},
                 ]],
             });
