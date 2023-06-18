0185d83ece49619e51374c0bddefc6d7434fec04
licha <licha@gmail.com>
2023-06-18 09:53:03 +0800

    js 更新

--- bot/static/admin/js/system/log.js
+++ bot/static/admin/js/system/log.js
@@ -15,9 +15,9 @@ define(["jquery", "easy-admin"], function ($, ea) {
                 toolbar: ['refresh'],
                 cols: [[
                     {field: 'month', title: '日志月份', hide: true, search: 'time', timeType: 'month', searchValue: util.toDateString(new Date(), 'yyyy-MM')},
-                    {field: 'groupname', minWidth: 80, title: '群组名', search: false},
-                    {field: 'username', minWidth: 80, title: '用户名', search: false},
-                    {field: 'name', minWidth: 80, title: '名字', search: false},
+                    {field: 'groupname', minWidth: 80, title: '群组名'},
+                    {field: 'username', minWidth: 80, title: '用户名'},
+                    {field: 'name', minWidth: 80, title: '名字'},
                     {width:200, field: 'url', minWidth: 80, title: '操作'},
                     {field: 'content', minWidth: 80, title: '操作内容'},
                     {field: 'ip', minWidth: 80, title: 'IP地址'},
