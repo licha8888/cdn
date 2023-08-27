03b977858e79fe39bb7d5d8a4c9d367cc81d01f2
licha <licha@gmail.com>
2023-08-27 23:02:03 +0800

    js更新

--- bot/static/admin/js/system/bot.js
+++ bot/static/admin/js/system/bot.js
@@ -17,15 +17,16 @@ define(["jquery", "easy-admin"], function ($, ea) {
             ea.table.render({
                 init: init,
                 cols: [[
-                    {field: 'botid', title: '机器人ID'},
+                    {width:108, field: 'botid', title: '机器人ID'},
                     {width:180, field: 'botnickname', title: '机器人名字'},
-                    {field: 'botusername', title: '机器人用户名'},
+                    {width:118, field: 'botusername', title: '机器人用户名'},
                     {field: 'adminname', title: '所属用户'},
                     {field: 'worktime', title: '日切时间'},
                     {field: 'showmode', title: '显示模式'},
+                    {field: 'autoclear', title: '自动日切', templet: ea.table.switch, search_ignore: true},
                     {field: 'spy', title: '监控', templet: ea.table.switch, search_ignore: true},
                     {field: 'status', title: '状态', templet: ea.table.switch, search_ignore: true},
-                    {field: 'create_time_text', title: '启动时间', search_ignore: true},
+                    {width:108, field: 'create_time_text', title: '启动时间', search_ignore: true},
                     {field: 'remark', title: '备注说明', templet: ea.table.text, search_ignore: true},
                     {title: '操作', templet: ea.table.tool, operat:['edit']}
                 ]],
