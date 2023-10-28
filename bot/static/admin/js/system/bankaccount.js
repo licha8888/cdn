f696cf180ef0ccabde88ead96725a1ad3801baa5
licha <licha@gmail.com>
2023-10-28 21:05:10 +0800

    js更新

--- bot/static/admin/js/system/bankaccount.js
+++ bot/static/admin/js/system/bankaccount.js
@@ -20,9 +20,12 @@ define(["jquery", "easy-admin"], function ($, ea) {
                 cols: [[
                     {width:180, field: 'botnickname', title: '机器人'},
                     {width:180, field: 'bankaccount', title: '银行卡号'},
-                    {field: 'accountinfo', title: '银行卡信息'},
+                    {width:180, field: 'bankname', title: '银行名称'},
+                    {width:145, field: 'cardtype', title: '卡类型'},
+                    {width:145, field: 'cardname', title: '卡名称'},
+                    {width:145, field: 'area', title: '所属地区'},
                     {width:180, field: 'create_time', title: '创建时间', search_ignore: true},
-                    {width: 115, title: '操作', templet: ea.table.tool},
+                    {title: '操作', templet: ea.table.tool},
 
                 ]],
             });
