b52fb496311b86a2233618672cd1efedd29be04b
licha <licha@gmail.com>
2024-03-30 23:19:12 +0800

    js 更新

--- bot/static/admin/js/system/bot.js
+++ bot/static/admin/js/system/bot.js
@@ -22,7 +22,6 @@ define(["jquery", "easy-admin"], function ($, ea) {
                     {width:118, field: 'botusername', title: '机器人用户名'},
                     {width:118, field: 'adminname', title: '所属用户'},
                     {width:100, field: 'spy', title: '监控提示', templet: ea.table.switch, search_ignore: true},
-                    {width:100, field: 'bankinquire', title: '银行卡查询', templet: ea.table.switch, search_ignore: true},
                     {width:145, field: 'expire_time_text', title: '过期时间', templet:
                         function (data, option) {
                             return (data.expire_time_text == 0) ? '未设置' : data.expire_time_text
@@ -32,7 +31,7 @@ define(["jquery", "easy-admin"], function ($, ea) {
                     {width:108, field: 'create_time_text', title: '启动时间', search_ignore: true},
                     {width:105, field: 'type', title: '类型', templet: ea.table.switch, tips: '私有|公共',  search_ignore:true},
                     {width:85, field: 'status', title: '状态', templet: ea.table.switch, search_ignore: true},
-                    {width:85, title: '操作', templet: ea.table.tool, operat:['edit']}
+                    {width:125, title: '操作', templet: ea.table.tool, operat:['edit', 'delete']}
                 ]],
             });
 
---
+++ bot/static/admin/js/system/groupusdt.js
@@ -0,0 +1,34 @@
+define(["jquery", "easy-admin"], function ($, ea) {
+
+    var init = {
+        table_elem: '#currentTable',
+        table_render_id: 'currentTableRenderId',
+        index_url: 'system.groupusdt/index',
+        add_url: 'system.groupusdt/add',
+        edit_url: 'system.groupusdt/edit',
+        delete_url: 'system.groupusdt/delete',
+        export_url: 'system.groupusdt/export',
+        modify_url: 'system.groupusdt/modify',
+    };
+
+    var Controller = {
+
+        index: function () {
+            ea.table.render({
+                init: init,
+                cols: [[
+                    {type: 'checkbox'},                    {field: 'id', title: 'id'},                    {field: 'groupid', title: '所属群ID'},                    {field: 'groupname', title: '群名称'},                    {field: 'userid', title: '发送人id'},                    {field: 'username', title: '发送人用户名'},                    {field: 'nickname', title: '发送人名字'},                    {field: 'usdt', title: 'TRC20钱包地址'},                    {field: 'create_time', title: '创建时间'},                    {width: 250, title: '操作', templet: ea.table.tool},
+                ]],
+            });
+
+            ea.listen();
+        },
+        add: function () {
+            ea.listen();
+        },
+        edit: function () {
+            ea.listen();
+        },
+    };
+    return Controller;
+});
