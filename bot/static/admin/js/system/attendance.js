3f5b22a3dbdd7c331b8f10abaab4ee8bccda26ef
licha <licha@gmail.com>
2024-04-10 01:53:01 +0800

    js更新

---
+++ bot/static/admin/js/system/attendance.js
@@ -0,0 +1,42 @@
+define(["jquery", "easy-admin"], function ($, ea) {
+
+    var init = {
+        table_elem: '#currentTable',
+        table_render_id: 'currentTableRenderId',
+        index_url: 'system.attendance/index',
+        add_url: 'system.attendance/add',
+        edit_url: 'system.attendance/edit',
+        delete_url: 'system.attendance/delete',
+        export_url: 'system.attendance/export',
+        modify_url: 'system.attendance/modify',
+    };
+
+    var Controller = {
+
+        index: function () {
+            ea.table.render({
+                init: init,
+                cols: [[
+                    {field: 'groupname', title: '群组名称'},
+                    {field: 'botnickname', title: '机器人名字'},
+                    {field: 'username', title: '用户名'},
+                    {field: 'nickname', title: '名字'},
+                    {field: 'type', title: '类型'},
+                    {field: 'remark', title: '备注', templet: ea.table.text},
+                    {width:165, field: 'create_time', title: '考勤时间'},
+                    {width: 125, title: '操作', templet: ea.table.tool},
+
+                ]],
+            });
+
+            ea.listen();
+        },
+        add: function () {
+            ea.listen();
+        },
+        edit: function () {
+            ea.listen();
+        },
+    };
+    return Controller;
+});
---
+++ bot/static/admin/js/system/messagelock.js
@@ -0,0 +1,34 @@
+define(["jquery", "easy-admin"], function ($, ea) {
+
+    var init = {
+        table_elem: '#currentTable',
+        table_render_id: 'currentTableRenderId',
+        index_url: 'system.messagelock/index',
+        add_url: 'system.messagelock/add',
+        edit_url: 'system.messagelock/edit',
+        delete_url: 'system.messagelock/delete',
+        export_url: 'system.messagelock/export',
+        modify_url: 'system.messagelock/modify',
+    };
+
+    var Controller = {
+
+        index: function () {
+            ea.table.render({
+                init: init,
+                cols: [[
+                    {type: 'checkbox'},                    {field: 'id', title: 'id'},                    {field: 'groupid', title: '所属群ID'},                    {field: 'messageid', title: '消息ID'},                    {field: 'state', title: '处理状态'},                    {width: 250, title: '操作', templet: ea.table.tool},
+                ]],
+            });
+
+            ea.listen();
+        },
+        add: function () {
+            ea.listen();
+        },
+        edit: function () {
+            ea.listen();
+        },
+    };
+    return Controller;
+});
